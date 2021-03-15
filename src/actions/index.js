278ee486fd806c1bbf20d72c533e69ddc4d6f053
Alexander <alex_sh_v@mail.ru>
2021-03-15 10:47:11 +0300

    fixed key

--- src/actions/index.js
+++ src/actions/index.js
@@ -6,7 +6,7 @@ export const FETCH_APOD_FAILURE = "FETCH_APOD_FAILURE";
 
 export const getApod = (date) => dispatch => {
     dispatch({ type: FETCH_APOD_START });
-    axios.get(`https://api.nasa.gov/planetary/apod?api_key=TNMgAwNaMhQ89vZJimj4EGVyvE528tchH23bYHf7${date !== "" ? "&date=" + date : ""}`)
+    axios.get(`https://api.nasa.gov/planetary/apod?api_key=${process.env.REACT_APP_NASA_API_KEY}${date !== "" ? "&date=" + date : ""}`)
     .then(res => dispatch({ type: FETCH_APOD_SUCCES, payload: res.data}))
     .catch(error => dispatch({ type: FETCH_APOD_FAILURE, payload: error.response.data.msg }))
 };
